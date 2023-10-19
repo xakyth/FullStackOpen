b9a435db2d3d8f79eae9ae0fca7706537e568702
xakyth <xakyth@gmail.com>
2023-10-19 19:39:40 +0300

    +ex1.2 and removed unnecessary fragments

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,28 @@
 const Header = (courseInfo) => {
   return (
-    <>
       <h1>{courseInfo.courseTitle}</h1>
-    </>
   )
 }
 
 const Content = (courseInfo) => {
   return (
-    <>
-      <p>
-        {courseInfo.partName} {courseInfo.exercisesNum}
-      </p>
-    </>
+    <div>
+      <Part name={courseInfo.part1} exercisesNum={courseInfo.exercises1} />
+      <Part name={courseInfo.part2} exercisesNum={courseInfo.exercises2} />
+      <Part name={courseInfo.part3} exercisesNum={courseInfo.exercises3} />
+    </div>
+  )
+}
+
+const Part = (part) => {
+  return (
+      <p>{part.name} {part.exercisesNum}</p>
   )
 }
 
 const Total = (courseInfo) => {
   return (
-    <>
       <p>Number of exercises {courseInfo.num}</p>
-    </>
   )
 }
 
@@ -36,6 +38,7 @@ const App = () => {
   return (
     <div>
       <Header courseTitle={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Content partName={part1} exercisesNum={exercises1} />
       <Content partName={part2} exercisesNum={exercises2} />
       <Content partName={part3} exercisesNum={exercises3} />
