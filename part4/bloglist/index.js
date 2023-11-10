17108ecd16c77c8df0186e5425a80c75803527ce
xakyth <xakyth@gmail.com>
2023-11-10 21:52:39 +0300

    added airbnb eslint to the proj

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,45 +1,46 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
-require('dotenv').config()
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+require('dotenv').config();
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl);
 
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+    .then((blogs) => {
+      response.json(blogs);
+    });
+});
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog(request.body);
 
   blog
     .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+    .then((result) => {
+      response.status(201).json(result);
+    });
+});
 
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
