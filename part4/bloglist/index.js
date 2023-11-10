c230d61f656190ecd034201f70a9d44e62bc3e1e
xakyth <xakyth@gmail.com>
2023-11-10 21:56:22 +0300

    added logger util

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const logger = require('./utils/logger');
 
 mongoose.set('strictQuery', false);
 require('dotenv').config();
@@ -42,5 +43,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
