53814bcd5a719213e378ef8b673b8712b89db876
xakyth <xakyth@gmail.com>
2023-11-11 09:23:45 +0300

    added blogs router

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
-const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blogs');
 
 mongoose.set('strictQuery', false);
 require('dotenv').config();
@@ -17,24 +17,8 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use('/api/blogs', blogsRouter);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
-});
 app.use(middleware.unknownEndPoint);
 app.use(middleware.errorHandler);
 
