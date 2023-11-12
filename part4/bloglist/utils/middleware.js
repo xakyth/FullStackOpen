474dac085b46fc23295bf71ed925fa8115578b34
xakyth <xakyth@gmail.com>
2023-11-12 12:48:56 +0300

    ifelse to switch const

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -11,10 +11,13 @@ const requestLogger = (request, response, next) => {
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+  switch (error) {
+    case 'CastError':
+      return response.status(400).send({ error: 'malformatted id' });
+    case 'ValidationError':
+      return response.status(400).json({ error: error.message });
+    default:
+      break;
   }
 
   next(error);
