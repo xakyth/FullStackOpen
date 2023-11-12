94f6f33fb6a5cd246690039a7b05ef8a2f6f2436
xakyth <xakyth@gmail.com>
2023-11-12 18:36:54 +0300

    done ex. 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,6 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => blogs.reduce((acc, cur) => acc + cur.likes, 0);
+
+module.exports = { dummy, totalLikes };
