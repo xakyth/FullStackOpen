c3683195de3d345339d6db3d1b04a459b15bca0d
xakyth <xakyth@gmail.com>
2023-11-15 18:53:06 +0300

    done ex 4.8

---
+++ part4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,21 @@
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const testHelper = require('./blogs_api_helper');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const blogObjects = testHelper.initialBlogs.map((b) => new Blog(b));
+  const promiseArray = blogObjects.map((b) => b.save());
+  await Promise.all(promiseArray);
+});
+
+test('get all blogs in json format', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+  expect(response.body).toHaveLength(testHelper.initialBlogs.length);
+});
