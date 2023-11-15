c901b127cbd3044e36db8a09504c2a85b38f54d6
xakyth <xakyth@gmail.com>
2023-11-15 20:47:14 +0300

    done ex. 4.10

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -26,3 +26,23 @@ test('unique identifier is "id"', async () => {
     expect(b.id).toBeDefined();
   });
 });
+
+test('successfull creation of a new blog', async () => {
+  const newBlog = {
+    title: 'Testing the backend',
+    author: 'Matti Luukkainen',
+    url: 'https://fullstackopen.com/en/part4/testing_the_backend',
+    likes: 0,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const getResponse = await api.get('/api/blogs');
+  expect(getResponse.body).toHaveLength(testHelper.initialBlogs.length + 1);
+  const urls = getResponse.body.map((b) => b.url);
+  expect(urls).toContain(newBlog.url);
+});
