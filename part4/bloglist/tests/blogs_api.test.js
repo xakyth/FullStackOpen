ffee0b22814fde35177d73f5919f02e5f682d912
xakyth <xakyth@gmail.com>
2023-11-16 06:53:40 +0300

    tests placed under describe blocks

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -11,84 +11,88 @@ beforeEach(async () => {
   await Blog.insertMany(testHelper.initialBlogs);
 });
 
-test('get all blogs in json format', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-  expect(response.body).toHaveLength(testHelper.initialBlogs.length);
-});
-
-test('unique identifier is "id"', async () => {
-  const response = await api.get('/api/blogs');
-  response.body.forEach((b) => {
-    expect(b.id).toBeDefined();
+describe('initially some blogs saved', () => {
+  test('get all blogs in json format', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+    expect(response.body).toHaveLength(testHelper.initialBlogs.length);
   });
-});
 
-test('successfull creation of a new blog', async () => {
-  const newBlog = {
-    title: 'Testing the backend',
-    author: 'Matti Luukkainen',
-    url: 'https://fullstackopen.com/en/part4/testing_the_backend',
-    likes: 0,
-  };
+  test('unique identifier is "id"', async () => {
+    const response = await api.get('/api/blogs');
+    response.body.forEach((b) => {
+      expect(b.id).toBeDefined();
+    });
+  });
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+  describe('addition of a blog', () => {
+    test('successfull creation of a new blog', async () => {
+      const newBlog = {
+        title: 'Testing the backend',
+        author: 'Matti Luukkainen',
+        url: 'https://fullstackopen.com/en/part4/testing_the_backend',
+        likes: 0,
+      };
 
-  const getResponse = await api.get('/api/blogs');
-  expect(getResponse.body).toHaveLength(testHelper.initialBlogs.length + 1);
-  const urls = getResponse.body.map((b) => b.url);
-  expect(urls).toContain(newBlog.url);
-});
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-test('create blog without likes specified', async () => {
-  const newBlog = {
-    title: 'Testing the backend',
-    author: 'Matti Luukkainen',
-    url: 'https://fullstackopen.com/en/part4/testing_the_backend',
-  };
+      const getResponse = await api.get('/api/blogs');
+      expect(getResponse.body).toHaveLength(testHelper.initialBlogs.length + 1);
+      const urls = getResponse.body.map((b) => b.url);
+      expect(urls).toContain(newBlog.url);
+    });
 
-  const postResponse = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-  expect(postResponse.body.likes).toEqual(0);
-});
+    test('create blog without likes specified', async () => {
+      const newBlog = {
+        title: 'Testing the backend',
+        author: 'Matti Luukkainen',
+        url: 'https://fullstackopen.com/en/part4/testing_the_backend',
+      };
 
-test('creation of blog without title or url will return 400 Bad Request', async () => {
-  const blogWithoutTitle = {
-    author: 'Matti Luukkainen',
-    url: 'https://fullstackopen.com/en/part4/testing_the_backend',
-  };
-  let postRequest = await api
-    .post('/api/blogs')
-    .send(blogWithoutTitle)
-    .expect(400);
-  expect(postRequest.body.message).toEqual('title cannot be empty');
-  const blogWithoutUrl = {
-    title: 'Testing the backend',
-    author: 'Matti Luukkainen',
-  };
+      const postResponse = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+      expect(postResponse.body.likes).toEqual(0);
+    });
 
-  postRequest = await api
-    .post('/api/blogs')
-    .send(blogWithoutUrl)
-    .expect(400);
-  expect(postRequest.body.message).toEqual('url cannot be empty');
+    test('creation of blog without title or url will return 400 Bad Request', async () => {
+      const blogWithoutTitle = {
+        author: 'Matti Luukkainen',
+        url: 'https://fullstackopen.com/en/part4/testing_the_backend',
+      };
+      let postRequest = await api
+        .post('/api/blogs')
+        .send(blogWithoutTitle)
+        .expect(400);
+      expect(postRequest.body.message).toEqual('title cannot be empty');
+      const blogWithoutUrl = {
+        title: 'Testing the backend',
+        author: 'Matti Luukkainen',
+      };
 
-  const blogWithoutTitleAndUrl = {
-    author: 'Matti Luukkainen',
-  };
-  postRequest = await api
-    .post('/api/blogs')
-    .send(blogWithoutTitleAndUrl)
-    .expect(400);
+      postRequest = await api
+        .post('/api/blogs')
+        .send(blogWithoutUrl)
+        .expect(400);
+      expect(postRequest.body.message).toEqual('url cannot be empty');
+
+      const blogWithoutTitleAndUrl = {
+        author: 'Matti Luukkainen',
+      };
+      postRequest = await api
+        .post('/api/blogs')
+        .send(blogWithoutTitleAndUrl)
+        .expect(400);
+    });
+  });
 });
 
 afterAll(async () => {
