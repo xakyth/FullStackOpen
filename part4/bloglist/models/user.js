48e2a5fdbd8297eccbb046ccea2d79001ba8138d
xakyth <xakyth@gmail.com>
2023-11-17 20:44:37 +0300

    added user model

---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+});
+
+userSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = doc._id.toString();
+    delete ret._id;
+    delete ret.passwordHash;
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
