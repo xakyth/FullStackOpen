eb00160c203a04b400e9b61b821309484e590346
xakyth <xakyth@gmail.com>
2023-11-18 11:30:25 +0300

    impl creating new users via POST request

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -1,9 +1,22 @@
+const bcrypt = require('bcrypt');
 const userRouter = require('express').Router();
 const User = require('../models/user');
 
 userRouter.get('/', async (request, response) => {
   const users = await User.find({});
   response.json(users);
+});
+
+userRouter.post('/', async (request, response, next) => {
+  const { username, name, password } = request.body;
+  const passwordHash = await bcrypt.hash(password, 10);
+  const userObject = new User({
+    username,
+    name,
+    passwordHash,
+  });
+  await userObject.save();
+  response.status(201).json(userObject);
 });
 
 module.exports = userRouter;
