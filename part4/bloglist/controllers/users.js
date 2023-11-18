309832498d0f23ab731accdbc95ea38db81e28a7
xakyth <xakyth@gmail.com>
2023-11-18 14:21:20 +0300

    now User's pwd shold be at least 3 char long

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const userRouter = require('express').Router();
 const User = require('../models/user');
+const { default: mongoose } = require('mongoose');
 
 userRouter.get('/', async (request, response) => {
   const users = await User.find({});
@@ -9,6 +10,12 @@ userRouter.get('/', async (request, response) => {
 
 userRouter.post('/', async (request, response, next) => {
   const { username, name, password } = request.body;
+  if (password.length < 3) {
+    let validationError = new mongoose.Error.ValidationError(null);
+    validationError.addError('password', new mongoose.Error.ValidatorError({ message: 'should contain at least 3 characters' }));
+    return next(validationError);
+  }
+  
   const passwordHash = await bcrypt.hash(password, 10);
   const userObject = new User({
     username,
--- part4/bloglist/tests/users_api.test.js
+++ part4/bloglist/tests/users_api.test.js
@@ -34,7 +34,7 @@ describe('initially there are some users in DB', () => {
         .send(newUser)
         .expect(201)
         .expect('Content-Type', /application\/json/);
-  
+
       const usersAtEnd = await helper.usersInDb();
       expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
       expect(
@@ -42,20 +42,36 @@ describe('initially there are some users in DB', () => {
       ).toBeDefined();
     });
     test('cannot create user with username less than 3 characters', async () => {
-        const usersAtStart = await helper.usersInDb();
-        const newUser = {
-          username: 'ki',
-          name: 'Denis Vasilyev',
-          password: '953457gwenj_gXX',
-        };
-        await api
-          .post('/api/users')
-          .send(newUser)
-          .expect(400)
-          .expect('Content-Type', /application\/json/);
-        
-        const usersAtEnd = await helper.usersInDb();
-        expect(usersAtStart).toHaveLength(usersAtEnd.length);
+      const usersAtStart = await helper.usersInDb();
+      const newUser = {
+        username: 'ki',
+        name: 'Denis Vasilyev',
+        password: '953457gwenj_gXX',
+      };
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/);
+
+      const usersAtEnd = await helper.usersInDb();
+      expect(usersAtStart).toHaveLength(usersAtEnd.length);
+    });
+    test('password must be at least 3 characters long', async () => {
+      const usersAtStart = await helper.usersInDb();
+      const newUser = {
+        username: 'kinowar',
+        name: 'Denis Vasilyev',
+        password: '95',
+      };
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/);
+
+      const usersAtEnd = await helper.usersInDb();
+      expect(usersAtStart).toHaveLength(usersAtEnd.length);
     });
   });
 });
