b13b4275a4d28a766d4efcb9b6591d9c0d46554d
xakyth <xakyth@gmail.com>
2023-11-26 18:49:59 +0300

    done ex. 5.7 individual blogs can be narrowed and expanded

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideWhenExpanded = { display: expanded ? 'none' : '' }
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
+  const toggleExpanded = () => setExpanded(!expanded)
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenExpanded}>
+        <div>{blog.title} <button onClick={toggleExpanded}>view</button></div>
+      </div>
+      <div style={showWhenExpanded}>
+        <div>{blog.title} <button onClick={toggleExpanded}>hide</button></div>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
