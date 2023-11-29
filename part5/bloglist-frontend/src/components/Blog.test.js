17069b5077d15fdf43c26a77eb34fe03655a033e
xakyth <xakyth@gmail.com>
2023-11-29 07:49:26 +0300

    done ex. 5.15. test, which ensures that if the like button is clicked twice, the event handler the component received as props is called twice.

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -5,33 +5,32 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  let container
-  beforeEach(() => {
-    const blog = {
-      title: '8blog title',
-      author: '8blog author',
-      url: '8http://tst2-123.ru',
-      likes: 10,
-      user: {
-        username: 'jojo',
-        name: 'Deniska',
-        id: '6558b254ae9f1ea3b8400030',
-      },
-      id: '655ba26500eebe8e4222b767',
-    }
-    const user = {
-      username: 'xakyth',
-      name: 'Roman',
-    }
-    container = render(<Blog
-        blog={blog}
-        user={user}
-        handleLike={() => null}
-        handleRemove={() => null}
-    />).container
-  })
+  const blog = {
+    title: '8blog title',
+    author: '8blog author',
+    url: '8http://tst2-123.ru',
+    likes: 10,
+    user: {
+      username: 'jojo',
+      name: 'Deniska',
+      id: '6558b254ae9f1ea3b8400030',
+    },
+    id: '655ba26500eebe8e4222b767',
+  }
+  const currentUser = {
+    username: 'xakyth',
+    name: 'Roman',
+  }
 
   test("blog's title and author rendered by default, but no URL likes user", () => {
+    const { container } = render(
+      <Blog
+        blog={blog}
+        user={currentUser}
+        handleLike={() => null}
+        handleRemove={() => null}
+      />
+    )
     const short = container.querySelector('.blogShort')
     const full = container.querySelector('.blogFull')
     expect(short).not.toHaveStyle('display: none')
@@ -39,13 +38,39 @@ describe('<Blog />', () => {
   })
 
   test('blog expands when view button is clicked', async () => {
+    const { container } = render(
+      <Blog
+        blog={blog}
+        user={currentUser}
+        handleLike={() => null}
+        handleRemove={() => null}
+      />
+    )
     const user = userEvent.setup()
     const short = container.querySelector('.blogShort')
     const full = container.querySelector('.blogFull')
     const viewButton = screen.getByText('view')
-    
+
     await user.click(viewButton)
     expect(short).toHaveStyle('display: none')
     expect(full).not.toHaveStyle('display: none')
+  })
+
+  test('if like button clicked twice, event handler recieve 2 events of like', async () => {
+    const handleLike = jest.fn()
+    const { container } = render(
+      <Blog
+        blog={blog}
+        user={currentUser}
+        handleLike={handleLike}
+        handleRemove={() => null}
+      />
+    )
+    const user = userEvent.setup()
+    const likeButton = screen.getByText('like')
+
+    await user.click(likeButton)
+    await user.click(likeButton)
+    expect(handleLike.mock.calls).toHaveLength(2)
   })
 })
