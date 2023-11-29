daeba55491dd97be5becd66140a0c2e6377386e7
xakyth <xakyth@gmail.com>
2023-11-29 07:33:25 +0300

    done ex. 5.14 Make a test, which checks that the blog's URL and number of likes are shown when the button controlling the shown details has been clicked.

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,38 +1,51 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  const blog = {
-    title: '8blog title',
-    author: '8blog author',
-    url: '8http://tst2-123.ru',
-    likes: 10,
-    user: {
-      username: 'jojo',
-      name: 'Deniska',
-      id: '6558b254ae9f1ea3b8400030',
-    },
-    id: '655ba26500eebe8e4222b767',
-  }
-  const user = {
-    username: 'xakyth',
-    name: 'Roman',
-  }
-
-  test("blog's title and author rendered by default, but no URL likes user", async () => {
-    const { container } = render(
-      <Blog
+  let container
+  beforeEach(() => {
+    const blog = {
+      title: '8blog title',
+      author: '8blog author',
+      url: '8http://tst2-123.ru',
+      likes: 10,
+      user: {
+        username: 'jojo',
+        name: 'Deniska',
+        id: '6558b254ae9f1ea3b8400030',
+      },
+      id: '655ba26500eebe8e4222b767',
+    }
+    const user = {
+      username: 'xakyth',
+      name: 'Roman',
+    }
+    container = render(<Blog
         blog={blog}
         user={user}
         handleLike={() => null}
         handleRemove={() => null}
-      />
-    )
+    />).container
+  })
+
+  test("blog's title and author rendered by default, but no URL likes user", () => {
     const short = container.querySelector('.blogShort')
     const full = container.querySelector('.blogFull')
     expect(short).not.toHaveStyle('display: none')
     expect(full).toHaveStyle('display: none')
+  })
+
+  test('blog expands when view button is clicked', async () => {
+    const user = userEvent.setup()
+    const short = container.querySelector('.blogShort')
+    const full = container.querySelector('.blogFull')
+    const viewButton = screen.getByText('view')
+    
+    await user.click(viewButton)
+    expect(short).toHaveStyle('display: none')
+    expect(full).not.toHaveStyle('display: none')
   })
 })
