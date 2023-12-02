c5f6a90aaa76b75152aae6594d17ff95c0f17d70
xakyth <xakyth@gmail.com>
2023-12-02 17:37:42 +0300

    added api to reset mongoDB

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -21,6 +21,10 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
