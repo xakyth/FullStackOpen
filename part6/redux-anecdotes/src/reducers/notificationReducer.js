ac6302f24de7065ac3751f8c94e07a20b79c8db3
xakyth <xakyth@gmail.com>
2023-12-10 13:39:37 +0300

    ex. 6.12 - added notification reducer

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: null,
+  reducers: {
+    setNotification(state, action) {
+      return action.payload
+    },
+    removeNotification() {
+      return null
+    },
+  },
+})
+
+export const { setNotification, removeNotification } =
+  notificationSlice.actions
+export default notificationSlice.reducer
--- part6/redux-anecdotes/src/reducers/store.js
+++ part6/redux-anecdotes/src/reducers/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import anecdotesReducer from './anecdoteReducer'
 import filterReducer from './filterReducer'
+import notificationReducer from './notificationReducer'
 
 const store = configureStore({
   reducer: {
     anecdotes: anecdotesReducer,
     filter: filterReducer,
+    notification: notificationReducer,
   },
 })
 
