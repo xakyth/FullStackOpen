b6a5076108a88c0da05b358e03b7415c57d59826
xakyth <xakyth@gmail.com>
2023-12-11 21:19:28 +0300

    ex. 6.16-6.17 - creation and initialization of anecdote refactored via redux thunk

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,20 +1,17 @@
 import { useDispatch } from 'react-redux'
-import { appendAnecdote } from '../reducers/anecdoteReducer'
+import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification, removeNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
   const addAnecdote = (event) => {
     event.preventDefault()
-    const anecdoteContent = event.target.content.value
+    const content = event.target.content.value
     event.target.content.value = ''
-    anecdoteService.createNew(anecdoteContent).then((anecdote) => {
-      dispatch(appendAnecdote(anecdote))
-    })
-
-    dispatch(setNotification(`you added anecdote: '${anecdoteContent}'`))
+    dispatch(createAnecdote(content))
+    
+    dispatch(setNotification(`you added anecdote: '${content}'`))
     setTimeout(() => {
       dispatch(removeNotification())
     }, 5000)
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdote',
@@ -20,4 +21,19 @@ const anecdoteSlice = createSlice({
 })
 
 export const { vote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
+  }
+}
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
