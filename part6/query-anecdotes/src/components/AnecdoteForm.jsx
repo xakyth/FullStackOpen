b31c03ee97aeb608fd00cba957e75b48f1ee0abd
xakyth <xakyth@gmail.com>
2023-12-14 19:28:18 +0300

    ex. 6.21 - new anecdotes via React Query

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,11 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createNew } from '../requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createNew,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  }
 
   return (
     <div>
---
+++ part6/query-anecdotes/src/requests.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAll = () => axios.get(baseUrl).then((res) => res.data)
+
+export const createNew = async (newAnecdote) => {
+  const res = await axios.post(baseUrl, newAnecdote)
+  return res.data
+}
