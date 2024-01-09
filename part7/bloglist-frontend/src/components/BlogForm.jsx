0076f4241788471f83538196bd0380859a877ffb
xakyth <xakyth@gmail.com>
2024-01-09 19:16:14 +0300

    BlogForm refactored according to redux

--- part7/bloglist-frontend/src/components/BlogForm.jsx
+++ part7/bloglist-frontend/src/components/BlogForm.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createBlog } from '../reducers/blogsReducer'
+import { setNotification } from '../reducers/notificationReducer'
+import NOTIFICATION_TYPE from '../constants/NotificationType'
 
-const BlogForm = ({ createBlog }) => {
+const BlogForm = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
+
+  const dispatch = useDispatch()
 
   const handleTitleChange = ({ target }) => setTitle(target.value)
   const handleAuthorChange = ({ target }) => setAuthor(target.value)
@@ -14,22 +20,56 @@ const BlogForm = ({ createBlog }) => {
     const blogObject = {
       title,
       author,
-      url
+      url,
     }
     setTitle('')
     setAuthor('')
     setUrl('')
-    createBlog(blogObject)
+    dispatch(createBlog(blogObject)).then((blog) => {
+      if (blog)
+        dispatch(
+          setNotification({
+            message: `a new blog ${blog.title} by ${blog.author} added`,
+            type: NOTIFICATION_TYPE.SUCCESS,
+          })
+        )
+    })
   }
 
   return (
     <div>
       <h2>create new</h2>
       <form onSubmit={submitBlog}>
-        <div>title:<input type="text" value={title} onChange={handleTitleChange} placeholder='Title'></input></div>
-        <div>author:<input type="text" value={author} onChange={handleAuthorChange} placeholder='Author'></input></div>
-        <div>url:<input type="text" value={url} onChange={handleUrlChange} placeholder='URL'></input></div>
-        <div><button type="submit">create</button></div>
+        <div>
+          title:
+          <input
+            type='text'
+            value={title}
+            onChange={handleTitleChange}
+            placeholder='Title'
+          ></input>
+        </div>
+        <div>
+          author:
+          <input
+            type='text'
+            value={author}
+            onChange={handleAuthorChange}
+            placeholder='Author'
+          ></input>
+        </div>
+        <div>
+          url:
+          <input
+            type='text'
+            value={url}
+            onChange={handleUrlChange}
+            placeholder='URL'
+          ></input>
+        </div>
+        <div>
+          <button type='submit'>create</button>
+        </div>
       </form>
     </div>
   )
