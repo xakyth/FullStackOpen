4e8f5eefbec6fef8a5bc43f5acff33a5c517ae8f
xakyth <xakyth@gmail.com>
2024-01-11 07:12:51 +0300

    added navigation menu styling

--- part7/bloglist-frontend/src/components/NavigationMenu.jsx
+++ part7/bloglist-frontend/src/components/NavigationMenu.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../reducers/loginReducer'
 import { Link } from 'react-router-dom'
+import { AppBar, Button, Toolbar, Typography } from '@mui/material'
 
 const NavigationMenu = () => {
   const dispatch = useDispatch()
@@ -16,14 +17,25 @@ const NavigationMenu = () => {
 
   return (
     <div>
-      <Link to={'/'} style={linkStyle}>
-        blogs
-      </Link>
-      <Link to={'/users'} style={linkStyle}>
-        users
-      </Link>
-      {user.name} logged in
-      <button onClick={handleLogout}>logout</button>
+      <AppBar position='static'>
+        <Toolbar>
+          <Button color='inherit' component={Link} to='/' variant='outlined'>
+            blogs
+          </Button>
+          <Button
+            color='inherit'
+            component={Link}
+            to='/users'
+            variant='outlined'
+          >
+            users
+          </Button>
+          <Typography sx={{ marginLeft: 'auto' }}>
+            {user.name} logged in
+            <button onClick={handleLogout}>logout</button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
