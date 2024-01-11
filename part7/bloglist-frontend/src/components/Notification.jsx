7706ead8d0ca39a3058684d320b6c11f0ee62937
xakyth <xakyth@gmail.com>
2024-01-11 06:47:57 +0300

    notification through @mui Alert component

--- part7/bloglist-frontend/src/components/Notification.jsx
+++ part7/bloglist-frontend/src/components/Notification.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux'
-import NOTIFICATION_TYPE from '../constants/NotificationType'
+import { Alert } from '@mui/material'
 
 const Notification = () => {
   const notification = useSelector((state) => state.notification)
@@ -7,32 +7,11 @@ const Notification = () => {
     return null
   }
   const { type, message } = notification
-  const inlineStyle = {
-    fontSize: '25px',
-    fontWeight: 'bold',
-    color: 'blue',
-    backgroundColor: '#d1ceca',
-    margin: '0',
-    border: '0.3rem solid',
-    borderRadius: '10px',
-    padding: '5px 10px',
-  }
-  switch (type) {
-    case NOTIFICATION_TYPE.ERROR:
-      inlineStyle.color = 'red'
-      break
-    case NOTIFICATION_TYPE.INFO:
-      inlineStyle.color = 'blue'
-      break
-    case NOTIFICATION_TYPE.SUCCESS:
-      inlineStyle.color = 'green'
-      break
-    default:
-      inlineStyle.color = 'blue'
-  }
   return (
     <div>
-      <p style={inlineStyle}>{message}</p>
+      <Alert color={type} variant='filled'>
+        {message}
+      </Alert>
     </div>
   )
 }
