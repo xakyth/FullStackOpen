95e301241e42b687c32b9747a8f65b8bf522bac4
xakyth <xakyth@gmail.com>
2024-02-08 19:22:48 +0300

    ex. 9.8 - init Patientor frontend + backend inital setup

---
+++ part9/patientor-backend/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone ping here');
+  res.send('pong');
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
