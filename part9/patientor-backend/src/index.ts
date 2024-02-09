4ab31ff838b27cf30bd2ff0698b0496b6b2e03c7
xakyth <xakyth@gmail.com>
2024-02-09 05:20:29 +0300

    ex. 9.10 - /api/diagnoses

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone ping here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 const PORT = 3001;
 
